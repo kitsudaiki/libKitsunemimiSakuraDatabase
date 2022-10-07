7ef73340c0bc8eee186d5711d073fd7194f72b34
Tobias Anker <tobias.anker@kitsunemimi.moe>
2022-10-07 14:34:43 +0200

    Style: cleanup build-script

--- build.sh
+++ build.sh
@@ -64,40 +64,13 @@ function get_required_private_repo_gitlab () {
     build_kitsune_lib_repo $REPO_NAME $NUMBER_OF_THREADS $ADDITIONAL_CONFIGS
 }
 
-function get_required_private_repo_github () {
-    REPO_NAME=$1
-    TAG_OR_BRANCH=$2
-    NUMBER_OF_THREADS=$3
-    ADDITIONAL_CONFIGS=$4
-
-    # clone repo
-    git clone https://kitsudaiki:$CLONE_TOKEN@github.com/kitsudaiki/$REPO_NAME.git "$PARENT_DIR/$REPO_NAME"
-    cd "$PARENT_DIR/$REPO_NAME"
-    git checkout $TAG_OR_BRANCH
-
-    build_kitsune_lib_repo $REPO_NAME $NUMBER_OF_THREADS  $ADDITIONAL_CONFIGS
-}
-
-function download_private_repo_github () {
-    REPO_NAME=$1
-    TAG_OR_BRANCH=$2
-
-    # clone repo
-    git clone https://kitsudaiki:$CLONE_TOKEN@github.com/kitsudaiki/$REPO_NAME.git "$BUILD_DIR/$REPO_NAME"
-    git clone https://kitsudaiki:$CLONE_TOKEN@github.com/kitsudaiki/$REPO_NAME.git "$PARENT_DIR/$REPO_NAME"
-    cd "$BUILD_DIR/$REPO_NAME"
-    git checkout $TAG_OR_BRANCH
-    cd "$PARENT_DIR/$REPO_NAME"
-    git checkout $TAG_OR_BRANCH
-}
-
 #-----------------------------------------------------------------------------------------------------------------
 
 get_required_kitsune_lib_repo "libKitsunemimiCommon" "v0.27.1" 8 "staticlib"
 
 get_required_kitsune_lib_repo "libKitsunemimiJson" "v0.12.0" 1 "staticlib"
 
-get_required_private_repo_github  "libKitsunemimiSqlite" "v0.4.0" 8 "staticlib"
+get_required_kitsune_lib_repo "libKitsunemimiSqlite" "v0.4.0" 8 "staticlib"
 
 #-----------------------------------------------------------------------------------------------------------------
 
